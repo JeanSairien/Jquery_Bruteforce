8957cad6c16c377ab6bcd623e2445b5ade663a56
mizer <reddeveloppement@wanalike.fr>
2016-07-31 15:05:46 +0200

    debut de resolution

---
+++ script.js
@@ -0,0 +1,11 @@
+$("button").click(function(){
+   $.ajax({url: "http://docusland.fr/code-academie/js_bruteforce/dico.txt", success: function(result){
+       $("#resultat").html(result);
+   }});
+});
+
+
+/*$("#resultat").load("dico.txt");*/
+
+
+
