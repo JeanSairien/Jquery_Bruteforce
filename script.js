447d759d31e3515ed22771d6dc5cd201f233ecbb
red <knoppix@Microknoppix.(none)>
2016-08-01 08:21:41 +0200

    resolution en cours

--- script.js
+++ script.js
@@ -4,8 +4,36 @@ $("button").click(function(){
    }});
 });
 
+var result = [$("#resultat").load("dico.txt");];
 
-/*$("#resultat").load("dico.txt");*/
 
+$(document).ready(function() {
+    // Lorsque je soumets le formulaire
+    $('#resultat').on('submit', function(e) {
+        e.preventDefault(); // J'empêche le comportement par défaut du navigateur, c-à-d de soumettre le formulaire
+ 
+        var $this = $(this); // L'objet jQuery du formulaire
+ 
+        // Je récupère les valeurs
 
+        var result = $('#resultat').val();
+        	for(i=0,i<6;i++);
+ 
+        // Je vérifie une première fois pour ne pas lancer la requête HTTP
+        // si je sais que mon PHP renverra une erreur
+        if(pseudo === '' || mail === '') {
+            alert('Les champs doivent êtres remplis');
+        } else {
+            // Envoi de la requête HTTP en mode asynchrone
+            $.ajax({
+                url: $this.attr('action'), // Le nom du fichier indiqué dans le formulaire
+                type: $this.attr('method'), // La méthode indiquée dans le formulaire (get ou post)
+                data: $this.serialize(), // Je sérialise les données (j'envoie toutes les valeurs présentes dans le formulaire)
+                success: function(html) { // Je récupère la réponse du fichier PHP
+                    alert(html); // J'affiche cette réponse
+                }
+            });
+        }
+    });
+});
 
