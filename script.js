950a5ad794f033d7f895cb4b53b491e6442d3c47
Redfish <lesonduko@gmail.com>
2016-08-03 16:30:31 +0200

    suite de la resolution en cours (syntax et methode approprier a trouver)

--- script.js
+++ script.js
@@ -1,44 +1,68 @@
-$("button").click(function(){
-   $.ajax({url: "http://docusland.fr/code-academie/js_bruteforce/dico.txt", success: function(result){
-       $("#resultat").html(result);
-   }});
-});
-
-var result = [$("#resultat").load("dico.txt");];
-
-//il semblerais qu'il faille incrementé le contenue du dico apres une methode http get ou post
-//dont je ne connait pas la syntax exact ... 
+var dico = [];
+var result = {};
 
 
-$(document).ready(function() {
-    // Lorsque je soumets le formulaire
-    $('#resultat').on('submit', function(e) {
-        e.preventDefault(); // J'empêche le comportement par défaut du navigateur, c-à-d de soumettre le formulaire
- 
-        var $this = $(this); // L'objet jQuery du formulaire
- 
-        // Je récupère les valeurs
 
-        var result = $('#resultat').val();
-        	for(i=0,i<6;i++);
- 
-        // Je vérifie une première fois pour ne pas lancer la requête HTTP
-        // si je sais que mon PHP renverra une erreur
-        if(pseudo === '' || mail === '') {
-            alert('Les champs doivent êtres remplis');
-        } else {
-            // Envoi de la requête HTTP en mode asynchrone
-            $.ajax({
-                url: $this.attr('action'), // Le nom du fichier indiqué dans le formulaire
-                type: $this.attr('method'), // La méthode indiquée dans le formulaire (get ou post)
-                data: $this.serialize(), // Je sérialise les données (j'envoie toutes les valeurs présentes dans le formulaire)
-                success: function(html) { // Je récupère la réponse du fichier PHP
-                    alert(html); // J'affiche cette réponse
-                }
-            });
+            
+    
+    $.ajax({
+       url:'http://docusland.fr/code-academie/js_bruteforce/data/dico.txt' ,// La ressource ciblée
+            complete: function(result){ 
+                dico = result.responseText.split("\n");
+					}
+                
+   							
+   							 
+                
+
+                console.log(dico.txt)
+
+
+              
+	var result = [$("#resultat").load("dico.txt");];  
+
+
+
+
+	
+function TraitPass(){
+
+	var i = 0;
+	do while (result =="Erroneous password");
+		url: 'http://docusland.fr/code-academie/js_bruteforce' ,//en envoie le resultat
+		type: "POST",
+		password: dico,
+		var i =dico[i];
+		i++;
+
+
+
+
+
+}
+
+
+
+
+
+function PassTest(password){
+        url : "http://docusland.fr/code-academie/js_bruteforce/index.php",
+        type: "POST",
+        password: password,
+        traitement: function(result){
+            
         }
-    });
-});
+
+}
+   
+
+
+//il semblerais qu'il faille incrementé le contenue du dico apres une methode http get 
+//dont je ne connait pas la syntax exact ... 
+//puis renvoyer le resultat apres incrementation au serveur avec methode post !!! 
+
+
+
 
 
 //http://chez-syl.fr/2012/01/envoyer-un-formulaire-en-ajax-avec-jquery-et-json/
